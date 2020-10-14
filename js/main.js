a0d49fa50f4917f2cabff3fa64a0849e8337ba47
Marco Giulietti <ga6t00n@gmail.com>
2020-10-14 13:52:57 +0200

    Job Done

---
+++ js/main.js
@@ -0,0 +1,49 @@
+/**
+ * Calcolatore di prezzo, secondo una tratta chilometrica
+ */
+
+// Raccolta dati:
+var prezzo = 0.21;
+var nome = prompt('Inserisci il tuo nome');
+
+document.getElementById('name').innerHTML = nome;
+
+
+
+var eta = parseInt( prompt('Inserisci la tua età') );
+
+//  Controllo età
+
+var chilo = parseInt( prompt('Inserire il numero di chilometri da fare') );
+
+//Controllo valore chilometri
+
+document.getElementById('chilo').innerHTML = chilo;
+
+// Costo biglietto per KM
+
+var costoKm = chilo * prezzo;
+var sconto20 = (costoKm / 100) * 20;
+var sconto40 = (costoKm / 100) * 40;
+
+// Calcolo di eventuali sconti
+
+if (eta < 18) {
+    var costoFinale = costoKm - sconto20;
+    document.getElementById('costo').innerHTML = costoFinale;
+    document.getElementById('tariffaInt').innerHTML = '(la tariffa intera per la sua tratta è pari a:<strong> ' + costoKm + '€</strong>)';
+    document.getElementById('sconto').innerHTML = 'A cui è stato applicato uno sconto del <strong>20%</strong> dedicato ai viaggiatori di età <em>inferiore ai 18 anni</em>!';
+} else if ( eta >= 65) {
+    var costoFinale = costoKm - sconto40;
+    document.getElementById('costo').innerHTML = costoFinale;
+    document.getElementById('tariffaInt').innerHTML = '(la tariffa intera per la sua tratta è pari a:<strong> ' + costoKm + '€</strong>)';
+    document.getElementById('sconto').innerHTML = 'A cui è stato applicato uno sconto del <strong>40%</strong> dedicato ai viaggiatori di età <em>superiore ai 65 anni</em>!';
+} else {
+    var costoFinale = costoKm
+    document.getElementById('costo').innerHTML = costoFinale;
+    document.getElementById('sconto').innerHTML = 'Siamo spiacenti, non è disponibile, al momento, una scontistica dedicata alla sua fascia di età!';
+}
+
+function newFunction() {
+    return 'Numero non valido! Inserisci un\'età valida, per favore!';
+}
